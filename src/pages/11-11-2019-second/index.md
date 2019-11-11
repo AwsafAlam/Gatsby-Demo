92eadafca500d927037c459aab668e711adc7d03
Md Awsaf Alam <1505114.maaa@ugrad.cse.buet.ac.bd>
2019-11-11 13:08:47 +0600

    Addde routing of blogs

---
+++ src/pages/11-11-2019-second/index.md
@@ -0,0 +1,23 @@
+---
+path: "/second"
+title: "Second"
+---
+
+# Hello
+
+Heyy this is a dynamically modified blog
+
+wow
+
+- nice
+- nice1
+- nice2
+
+`dl`
+
+```C++
+void func(int a, int b){
+ return a +b;
+}
+
+```
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
@@ -14,8 +14,29 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    {
+      data.allMarkdownRemark.edges.map(post => (
+        <a key={post.node.id} href={post.node.frontmatter.path}>{post.node.frontmatter.title}</a>
+      ))
+    }
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
+
+export const pageQuery = graphql `
+  query IndexQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            path
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
