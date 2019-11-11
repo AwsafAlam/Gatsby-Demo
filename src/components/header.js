41cb99ab28d200f87ce15e83dc90ffda502c1692
Md Awsaf Alam <1505114.maaa@ugrad.cse.buet.ac.bd>
2019-11-11 20:02:01 +0600

    Resume added

--- src/components/header.js
+++ src/components/header.js
@@ -4,44 +4,44 @@ import React from "react"
 import Image from './image'
 
 const Header = ({ siteTitle }) => (
-    <header class="header text-center">	    
-	    <h1 class="blog-name pt-lg-4 mb-0"><a href="index.html">{siteTitle}</a></h1>
+    <header className="header text-center">	    
+	    <h1 className="blog-name pt-lg-4 mb-0"><a href="index.html">{siteTitle}</a></h1>
         
-	    <nav class="navbar navbar-expand-lg navbar-dark" >
+	    <nav className="navbar navbar-expand-lg navbar-dark" >
            
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
+			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+			<span className="navbar-toggler-icon"></span>
 			</button>
 
-			<div id="navigation" class="collapse navbar-collapse flex-column" >
-				<div class="profile-section pt-3 pt-lg-0">
-				    <Image class="profile-image mb-3 rounded-circle mx-auto" alt="image" />			
+			<div id="navigation" className="collapse navbar-collapse flex-column" >
+				<div className="profile-section pt-3 pt-lg-0">
+				    <Image className="profile-image mb-3 rounded-circle mx-auto" alt="image" />			
 					
-					<div class="bio mb-3">Hi, my name is Anthony Doe. Briefly introduce yourself here. You can also provide a link to the about page.<br/><a href="about.html">Find out more about me</a></div>
-					<ul class="social-list list-inline py-3 mx-auto">
-			            {/* <li class="list-inline-item"><a href="https://www."><i class="fab fa-twitter fa-fw"></i></a></li> */}
-			            <li class="list-inline-item"><a href="https://www.linkedin.com/in/awsafalam"><i class="fab fa-linkedin-in fa-fw"></i></a></li>
-			            <li class="list-inline-item"><a href="https://www.github.com/AwsafAlam"><i class="fab fa-github-alt fa-fw"></i></a></li>
-			            {/* <li class="list-inline-item"><a href="https://www."><i class="fab fa-stack-overflow fa-fw"></i></a></li> */}
-			            <li class="list-inline-item"><a href="https://www.facebook.com/awsafanindo"><i class="fab fa-codepen fa-fw"></i></a></li>
+					<div className="bio mb-3">Hi, my name is Awsaf Alam. Briefly introduce yourself here. You can also provide a link to the about page.<br/><a href="about.html">Find out more about me</a></div>
+					<ul className="social-list list-inline py-3 mx-auto">
+			            {/* <li className="list-inline-item"><a href="https://www."><i className="fab fa-twitter fa-fw"></i></a></li> */}
+			            <li className="list-inline-item"><a href="https://www.linkedin.com/in/awsafalam"><i className="fab fa-linkedin-in fa-fw"></i></a></li>
+			            <li className="list-inline-item"><a href="https://www.github.com/AwsafAlam"><i className="fab fa-github-alt fa-fw"></i></a></li>
+			            {/* <li className="list-inline-item"><a href="https://www."><i className="fab fa-stack-overflow fa-fw"></i></a></li> */}
+			            <li className="list-inline-item"><a href="https://www.facebook.com/awsafanindo"><i className="fab fa-codepen fa-fw"></i></a></li>
 			        </ul>
 			        <hr/> 
 				</div>
 				
-				<ul class="navbar-nav flex-column text-left">
-					<li class="nav-item active">
-					    <a class="nav-link" href="index.html"><i class="fas fa-home fa-fw mr-2"></i>Home <span class="sr-only">(current)</span></a>
+				<ul className="navbar-nav flex-column text-left">
+					<li className="nav-item active">
+					    <Link className="nav-link" to="/"><i className="fas fa-home fa-fw mr-2"></i>Home <span className="sr-only">(current)</span></Link>
 					</li>
-					<li class="nav-item">
-					    <Link class="nav-link" to="/blog"><i class="fas fa-bookmark fa-fw mr-2"></i>Blog</Link>
+					<li className="nav-item">
+					    <Link className="nav-link" to="/"><i className="fas fa-bookmark fa-fw mr-2"></i>Blog</Link>
 					</li>
-					<li class="nav-item">
-					    <Link class="nav-link" to="/about"><i class="fas fa-user fa-fw mr-2"></i>About Me</Link>
+					<li className="nav-item">
+					    <Link className="nav-link" to="/about"><i className="fas fa-user fa-fw mr-2"></i>About Me</Link>
 					</li>
 				</ul>
 				
-				<div class="my-2 my-md-3">
-				    <a class="btn btn-primary" href="https://facebook.com/awsaf.anindo" rel="noopener noreferrer" target="_blank">Get in Touch</a>
+				<div className="my-2 my-md-3">
+				    <a className="btn btn-primary" href="https://facebook.com/awsaf.anindo" rel="noopener noreferrer" target="_blank">Get in Touch</a>
 				</div>
 			</div>
 		</nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,33 +9,33 @@ const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
     
-	    <section class="cta-section theme-bg-light py-5">
-		    <div class="container text-center">
-			    <h2 class="heading">Hi, I'm Awsaf</h2>
-			    <div class="intro">Welcome to my blog. Subscribe and get my latest blog post in your inbox.</div>
-			    <form class="signup-form form-inline justify-content-center pt-3">
-              <div class="form-group">
-                  <label class="sr-only" for="semail">Your email</label>
-                  <input type="email" id="semail" name="semail1" class="form-control mr-md-1 semail" placeholder="Enter email"/>
+	    <section className="cta-section theme-bg-light py-5">
+		    <div className="container text-center">
+			    <h2 className="heading">Hi, I'm Awsaf</h2>
+			    <div className="intro">Welcome to my blog. Subscribe and get my latest blog post in your inbox.</div>
+			    <form className="signup-form form-inline justify-content-center pt-3">
+              <div className="form-group">
+                  <label className="sr-only" for="semail">Your email</label>
+                  <input type="email" id="semail" name="semail1" className="form-control mr-md-1 semail" placeholder="Enter email"/>
               </div>
-              <button type="submit" class="btn btn-primary">Subscribe</button>
+              <button type="submit" className="btn btn-primary">Subscribe</button>
           </form>
 		    </div>
 	    </section>
-	    <section class="blog-list px-3 py-5 p-md-5">
-		    <div class="container">
+	    <section className="blog-list px-3 py-5 p-md-5">
+		    <div className="container">
         {
           data.allMarkdownRemark.edges.map(post => (
-            <div key={post.node.id} class="item mb-5">
-              <div class="media">
-                <Image class="mr-3 Image-fluid post-thumb d-none d-md-flex" src="assets/images/blog/blog-post-thumb-1.jpg" alt="image"/>
-                <div class="media-body">
-                  <h3 class="title mb-1"><Link to={post.node.frontmatter.path}>{post.node.frontmatter.title}</Link></h3>
-                  <div class="meta mb-1"><span class="date">Published {post.node.frontmatter.date}</span><span class="time">5 min read</span><span class="comment">
+            <div key={post.node.id} className="item mb-5">
+              <div className="media">
+                <Image className="mr-3 Image-fluid post-thumb d-none d-md-flex" src="assets/images/blog/blog-post-thumb-1.jpg" alt="image"/>
+                <div className="media-body">
+                  <h3 className="title mb-1"><Link to={post.node.frontmatter.path}>{post.node.frontmatter.title}</Link></h3>
+                  <div className="meta mb-1"><span className="date">Published {post.node.frontmatter.date}</span><span className="time">5 min read</span><span className="comment">
                     {/* <a href="#">8 comments</a> */}
                   </span></div>
-                    <div class="intro">{post.node.frontmatter.description}</div>
-                  <Link class="more-link" to={post.node.frontmatter.path}>Read more &rarr;</Link>
+                    <div className="intro">{post.node.frontmatter.description}</div>
+                  <Link className="more-link" to={post.node.frontmatter.path}>Read more &rarr;</Link>
                 </div>
               </div>
             </div>
@@ -43,17 +43,17 @@ const IndexPage = ({data}) => (
           ))
         }
 			    
-        <nav class="blog-nav nav nav-justified my-5">
-				  {/* <a class="nav-link-prev nav-item nav-link d-none rounded-left" href="#">Previous<i class="arrow-prev fas fa-long-arrow-alt-left"></i></a>
-				  <a class="nav-link-next nav-item nav-link rounded" href="blog-list.html">Next<i class="arrow-next fas fa-long-arrow-alt-right"></i></a> */}
+        <nav className="blog-nav nav nav-justified my-5">
+				  {/* <a className="nav-link-prev nav-item nav-link d-none rounded-left" href="#">Previous<i className="arrow-prev fas fa-long-arrow-alt-left"></i></a>
+				  <a className="nav-link-next nav-item nav-link rounded" href="blog-list.html">Next<i className="arrow-next fas fa-long-arrow-alt-right"></i></a> */}
 				</nav>
 				
 		    </div>
 	    </section>
 	    
-	    <footer class="footer text-center py-2 theme-bg-dark">
+	    <footer className="footer text-center py-2 theme-bg-dark">
 		   
-      {/* <small class="copyright">Made with <i class="fas fa-heart" style="color: #fb866a;"></i> by <a href="https://awsafdev.netlify.com" target="_blank">Awsaf</a></small> */}
+      {/* <small className="copyright">Made with <i className="fas fa-heart" style="color: #fb866a;"></i> by <a href="https://awsafdev.netlify.com" target="_blank">Awsaf</a></small> */}
 		   
 	    </footer>
   </Layout>
